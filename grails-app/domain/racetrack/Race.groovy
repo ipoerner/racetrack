c2a5f81f5d062bfb5e4c1cef9390d599a3887f38
Ingmar Poerner <ipoerner@gmail.com>
2012-12-02 20:30:08 +0100

    add constraints for Race domain class

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -1,6 +1,16 @@
 package racetrack
 
 class Race {
+    static constraints = {
+        name(blank:false, maxSize:50)
+        startDate()
+        city()
+        state(inList:["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:100.0)
+        maxRunners(min:0, max:100000)
+    }
+
     String name
     Date startDate
     String city
@@ -8,7 +18,4 @@ class Race {
     BigDecimal distance
     BigDecimal cost
     Integer maxRunners = 100000
-    
-    static constraints = {
-    }
 }
