f1321ffb63abd5b50def110b1a912b9d10e7e230
Ingmar Poerner <ipoerner@gmail.com>
2012-12-02 22:31:23 +0100

    split Registration domain class up into Registration and Runner

--- grails-app/domain/racetrack/Registration.groovy
+++ grails-app/domain/racetrack/Registration.groovy
@@ -1,18 +1,15 @@
 package racetrack
 
 class Registration {
-    String name
-    Date dateOfBirth
-    String gender
-    String address
-    String city
-    String state
-    String zipcode
-    String email
-    Date dateCreated //Note: this is a special name
-
-    static belongsTo = [race:Race]
-
     static constraints = {
+        race()
+        runner()
+        paid()
+        dateCreated()
     }
+
+    static belongsTo = [race:Race, runner:Runner]
+
+    Boolean paid
+    Date dateCreated
 }
