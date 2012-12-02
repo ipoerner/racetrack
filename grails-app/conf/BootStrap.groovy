73628cb6ae8ad40858ed6af2ca1a52a1e002d77d
Ingmar Poerner <ipoerner@gmail.com>
2012-12-02 22:41:59 +0100

    bootstrap Race and Registration in development mode only

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,20 +1,52 @@
+import grails.util.GrailsUtil
+
 class BootStrap {
 
     def init = { servletContext ->
-        def jane = new racetrack.Runner(
-            firstName:"Jane",
-            lastName:"Doe",
-            dateOfBirth:(new Date() - 365*30),
-            gender:"F",
-            address:"123 Main St",
-            city:"Goose",
-            state:"NC",
-            zipcode:"12345",
-            email:"jane@whereever.com"
-            )
-        jane.save()
-        if(jane.hasErrors()){
-            println jane.errors
+        switch(GrailsUtil.environment){
+            case "development":
+                def jane = new racetrack.Runner(
+                    firstName:"Jane",
+                    lastName:"Doe",
+                    dateOfBirth:(new Date() - 365*30),
+                    gender:"F",
+                    address:"123 Main St",
+                    city:"Goose",
+                    state:"NC",
+                    zipcode:"12345",
+                    email:"jane@whereever.com"
+                    )
+                jane.save()
+                if(jane.hasErrors()){
+                    println jane.errors
+                }
+
+                def trot = new racetrack.Race(
+                    name:"Turkey Trot",
+                    startDate:(new Date() + 90),
+                    city:"Duck",
+                    state:"NC",
+                    distance:5.0,
+                    cost:20.0,
+                    maxRunners:350
+                    )
+                trot.save()
+                if(trot.hasErrors()){
+                    println trot.errors
+                }
+
+                def reg = new racetrack.Registration(
+                    paid:false,
+                    runner:jane,
+                    race:trot
+                    )
+                reg.save()
+                if(reg.hasErrors()){
+                    println reg.errors
+                }
+            break
+
+            case "production" : break
         }
     }
 
